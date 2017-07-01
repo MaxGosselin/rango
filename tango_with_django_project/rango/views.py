604c90914d3c1afe42b2050a5dd261ed92f82720
Max Gosselin <max.jp.gosselin@gmail.com>
2017-07-01 00:31:56 -0400

    Done for now, at begining of ch.4

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("""rango says please clap
+
+    <br/> <a href='/rango/about/'>About</a>""")
+
+def about(request):
+    return HttpResponse("""rango says i'm about it
+    <br/> <a href='/rango/'>Index</a>""")
