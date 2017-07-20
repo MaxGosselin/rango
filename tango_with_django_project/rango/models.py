c93887679b4d35df2d1e79c7f74a13099f8376c0
Max Gosselin <max.jp.gosselin@gmail.com>
2017-07-20 01:39:34 -0400

    chapter 5 excercise up to admin stuff

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -7,6 +7,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -20,6 +22,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
