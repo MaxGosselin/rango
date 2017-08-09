f5289638cdc20213009cccb33b385a8fb17bc802
Max Gosselin <max.jp.gosselin@gmail.com>
2017-08-09 16:31:26 -0400

    Tinkered for half an hour, will come back after a bike ride.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+from rango.models import Category, Page
+
 def index(request):
+
+
+    # Get a list of all categories from the db.
+    # Pass top 5 -- by likes -- to the context dict
+    category_list = Category.objects.order_by('likes')[:5]
+
     #dict for template engine
-    #note: boldmessage is same as {{ boldmessage }} in the template
-    context_dict = {'boldmessage': "Will you PLEASE clap? :("}
+    context_dict = {'categories': category_list}
 
     #return a rendered response; first param is the template we want to use
     return render(request, 'rango/index.html', context=context_dict)
@@ -19,3 +24,28 @@ def about(request):
             }
 
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    # Create a context dict
+    context_dict = {}
+
+    try:
+        # Raises DoesNotExist except on fail.
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Get all assosciated pages.
+        pages = Page.objects.filter(category=category)
+
+        # Add pages to c_d
+        context_dict['pages'] = pages
+
+        # Also pass the category
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+
